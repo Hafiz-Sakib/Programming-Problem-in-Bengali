7423c168e879f2e5bc0bcbdc0fa40cc5eaa723ec
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-17 22:16:54 +0600

    Previous ICPC Questions

---
+++ ICPC-Questions-and-Answer-Using-CPP/Mock of ICPC Dhaka Regional 2020 Online Preliminary/A. Almost Forgot to Welcome.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+/*
+*   SUB-Subsequence
+*   Team Member:
+*   Aryan Kabir
+*   Nymul Islam Moon
+*   Majharul Islam
+*
+*/
+int main()
+    {
+
+        cout<<"Welcome to ICPC Dhaka Regional Online Preliminary Contest, 2019\n";
+
+    }
